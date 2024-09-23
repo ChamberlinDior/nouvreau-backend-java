e6dbd660a4558b4abcd0a8b7447d5d62f74475e4
ChamberlinDior <chamberlindior@gmail.com>
2024-09-23 07:59:53 -0400

    fg

--- trans/src/main/java/com/bus/trans/TransApplication.java
+++ trans/src/main/java/com/bus/trans/TransApplication.java
@@ -3,10 +3,11 @@ package com.bus.trans;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@EntityScan(basePackages = "com.bus.trans.model")
 @SpringBootApplication
 public class TransApplication implements CommandLineRunner {
 
